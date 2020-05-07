71abea21fb583fdaabc80c64e3441323a7570c53
Mark Kruse <1224221+krusem@users.noreply.github.com>
2020-05-07 15:15:39 -0400

    Update
    Added non domain joined auth and ability to check for internal endpoint since the saml endpoint should be internal and not public.

--- Start-AWSCreds.ps1
+++ Start-AWSCreds.ps1
@@ -1,10 +1,19 @@
 function Start-AWSCreds {
-    $endpoint = "<SAML_ENDPOINT>";
-    $epName = Set-AWSSamlEndpoint -Endpoint $endpoint -StoreAs ADFS -AuthenticationType NTLM;
-    $list = Set-AWSSamlRoleProfile -StoreAll -EndpointName $epName
-    $printlist = @()
-    for($i=1; $i -le $list.length; $i++) { $printlist += "&$i - " + $list[$i-1] }
-    $opt = [System.Management.Automation.Host.ChoiceDescription[]] $printlist
-    $res = $host.UI.PromptForChoice("AWS Role","Which role would you like to assume?",$opt,0)
-    Set-AWSCredential -ProfileName $list[$res] -Scope Global
+    if(Test-Connection <INTERNAL_ENDPOINT> -quiet -count 1) {
+        $endpoint = "<SAML_ENDPOINT";
+        $epName = Set-AWSSamlEndpoint -Endpoint $endpoint -StoreAs ADFS -AuthenticationType NTLM;
+        if((Get-WmiObject -Class Win32_ComputerSystem).PartOfDomain) {
+            $list = Set-AWSSamlRoleProfile -StoreAll -EndpointName $epName
+        } else {
+            $credential = Get-Credential -Message "Enter the domain credentials for the endpoint"
+            $list = Set-AWSSamlRoleProfile -StoreAll -EndpointName $epName -NetworkCredential $credential
+        }
+        $printlist = @()
+        for($i=1; $i -le $list.length; $i++) { $printlist += "&$i - " + $list[$i-1] }
+        $opt = [System.Management.Automation.Host.ChoiceDescription[]] $printlist
+        $res = $host.UI.PromptForChoice("AWS Role","Which role would you like to assume?",$opt,0)
+        Set-AWSCredential -ProfileName $list[$res] -Scope Global
+    } else {
+        write-host "Could not connect to internal endpoint, please confirm your VPN is active."
+    }
 }
